b1bd729d10a355904c0415d8fa1370b4795e8ca4
donna <donnaxnguyen@yahoo.com>
2020-08-14 02:19:59 -0700

    changing port

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const path = require('path');
 
 // express function on port
 const app = express()
-const port = process.env.PORT || 3000
+// const port = process.env.PORT || 3000
+const port = process.env.PUBLIC_URL || 3000
+
 
 // using express 
 app.use(express.static(path.join(__dirname, 'build')));
