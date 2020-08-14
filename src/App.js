0621fd73f3d949ed519afedadeeae0cf34be9f83
donna <donnaxnguyen@yahoo.com>
2020-08-13 23:45:10 -0700

    added styling to employees.css and search.css, imported directory to app.js, installed moment because it isn't working in terminal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Directory from "./components/directory";
 
 // running the app .. doing the magic ..
 // grabbing class directory from components folder  
