26fe8e028ec44b1326956506bd271255f3479772
donna <donnaxnguyen@yahoo.com>
2020-08-13 22:29:51 -0700

    changed title in index.html and started working on employees.js

---
+++ src/components/employees.js
@@ -0,0 +1,23 @@
+// requiring react and moment
+import React from "react";
+import Moment from "moment";
+// ********* adding .css styling later ********* //
+
+// employees will be exported and props will be used to render through the components
+const Employees = (props) => {
+    // fixedDate will use Moment for their date of birth
+    let fixedDate = Moment(props.dob).format("LL");
+
+    // creating a table row (tr) and table data (td)
+    return (
+    // will show first name , last name , icon , email , phone , and birthdate
+      <tr className="tr"> 
+        <td><img alt={props.firstName} src={props.icon} /></td>
+        <td>{props.firstName} {props.lastName}</td>
+        <td>{props.email}</td>
+        <td>{props.phone} </td>
+        <td>{fixedDate}</td>
+      </tr>
+    )}
+
+  export default Employees;
