ae77bfd5783b572ea9838a11aad8e3cdc5be94ed
donna <donnaxnguyen@yahoo.com>
2020-08-14 02:22:27 -0700

    playing with app.js

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-// requiring npm express and npm path
+// // requiring npm express and npm path
+// const express = require('express')
+// const path = require('path');
+
+// // express function on port
+// const app = express()
+// // const port = process.env.PORT || 3000
+// const port = process.env.PUBLIC_URL || 3000
+
+
+// // using express 
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.listen(port, () => console.log(`App is running on ${port}!`))
+
+
 const express = require('express')
-const path = require('path');
-
-// express function on port
+const path = require('path')
+const port = process.env.PORT || 8080
 const app = express()
-// const port = process.env.PORT || 3000
-const port = process.env.PUBLIC_URL || 3000
 
+// this assumes that all your app files
 
-// using express 
-app.use(express.static(path.join(__dirname, 'build')));
-app.listen(port, () => console.log(`App is running on ${port}!`))
+app.use(express.static(__dirname + '/public'))
+
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
+app.listen(port)
+console.log("Server started on port " + port)
