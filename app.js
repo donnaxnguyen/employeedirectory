bb32ab3f6d2976e54fac6709908d7c6aae403e32
donna <donnaxnguyen@yahoo.com>
2020-08-14 00:37:12 -0700

    trying to get heroku to work

--- app.js
+++ app.js
@@ -8,4 +8,8 @@ const port = process.env.PORT || '3000';
 
 // using express 
 app.use(express.static(path.join(__dirname, 'build')));
-app.listen(port, () => console.log(`App is running on ${port}!`))
+// app.listen(port, () => console.log(`App is running on ${port}!`))
+
+app.listen( app.get('port'), function() {
+    console.log( 'App is running on port ' + app.get( 'port' ));
+    });
