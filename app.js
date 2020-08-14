6d2075cd198d956421d76e856895d8a9574262fa
donna <donnaxnguyen@yahoo.com>
2020-08-14 14:49:23 -0700

    moving html outside

--- app.js
+++ app.js
@@ -15,10 +15,9 @@
 
 const express = require('express')
 const path = require('path')
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3000
 const app = express()
 
-// this assumes that all your app files
 
 app.use(express.static(__dirname + '/public'))
 
