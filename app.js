188a96553c0ffece553446bbe2739a8290f284a4
donna <donnaxnguyen@yahoo.com>
2020-08-14 00:29:09 -0700

    tweaking package.json

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // express function on port
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || '3000';
 
 // using express 
 app.use(express.static(path.join(__dirname, 'build')));
