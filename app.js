ce98f410631b3e7bbe1c55b0af16cd71ff3be188
donna <donnaxnguyen@yahoo.com>
2020-08-13 22:06:42 -0700

    adding express and path, also added app.js to run on port

---
+++ app.js
@@ -0,0 +1,11 @@
+// requiring npm express and npm path
+const express = require('express')
+const path = require('path');
+
+// express function on port
+const app = express()
+const port = process.env.PORT || 3000
+
+// using express 
+app.use(express.static(path.join(__dirname, 'build')));
+app.listen(port, () => console.log(`App is running on ${port}!`))
